19e605d9c9836a6f704cda8a156bc9735da465d9
Dmitry Krayniy <krdima92@gmail.com>
2022-08-12 16:44:34 +0400

    Updated pipeline

--- build.ps1
+++ build.ps1
@@ -3,37 +3,29 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-
-# Get buildnumber from github actions
-if ($env:GITHUB_RUN_NUMBER -ne $null) {
-    $component.build = $env:GITHUB_RUN_NUMBER
-    Set-Content -Path "component.json" -Value $($component | ConvertTo-Json)
-}
-
-$buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
-$container=$component.name
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$buildImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
+$container = $component.name
 
 # Remove build files
-if (Test-Path "obj") {
-    Remove-Item -Recurse -Force -Path "obj"
+if (Test-Path -Path "$PSScriptRoot/obj") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/obj"
 }
-if (Test-Path "lib") {
-    Remove-Item -Recurse -Force -Path "lib"
+if (Test-Path -Path "$PSScriptRoot/lib") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/lib"
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.build -t $buildImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.build" -t $buildImage .
 
 # Create and copy compiled files, then destroy
 docker create --name $container $buildImage
-docker cp "$($container):/app/obj" ./obj
-docker cp "$($container):/app/lib" ./lib
+docker cp "$($container):/app/obj" "$PSScriptRoot/obj"
+docker cp "$($container):/app/lib" "$PSScriptRoot/lib"
 docker rm $container
 
 # Verify build
-if (!(Test-Path ./obj) -or !(Test-Path ./lib)) {
-    Write-Host "obj or lib folder doesn't exists in root dir. Build failed. Watch logs above."
-    exit 1
+if (-not (Test-Path -Path "$PSScriptRoot/obj") -or -not (Test-Path -Path "$PSScriptRoot/lib")) {
+    Write-Error "obj or lib folder doesn't exists in root dir. Build failed. See logs above for more information."
 }
